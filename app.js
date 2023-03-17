7636f218af45d29a0bd394274e48e9ff36dcb8ad
madtoshi <106340158+madtoshi@users.noreply.github.com>
2023-03-17 10:29:31 -0400

    completed v1 of app.js

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
-let quotes = {
-    a: "quote",
-    b: "next",
-    c: "final"
-};
+import fetch from "node-fetch";
+
+const api_url ="https://zenquotes.io/api/quotes/";
+
+async function getapi(url) {
+  const response = await fetch(url);
+  var data = await response.json();
+  return data;
+}
+
+function displayQuote(quote, author) {
+    console.log(`${quote} -${author}`)
+}
+
+function selectRandomQuote(arr) {
+    const random = Math.floor(Math.random() * arr.length - 1);
+    return random;
+}
+
+function getQuote(arr) {
+    const random = selectRandomQuote(arr);
+    return displayQuote(arr[random].q, arr[random].a);
+}
+
+let quotes = await getapi(api_url);
+getQuote(quotes);
